b53ad74a95c0219c9534b6007ccbfde0468faead
Tomás Farías <83795131+SimuladorDeFarm@users.noreply.github.com>
2024-10-24 13:37:52 -0300

    Update install_linux_script.sh

--- install_linux_script.sh
+++ install_linux_script.sh
@@ -1,11 +1,5 @@
 #! /bin/bash
 
-#clonar repositorio
-git clone https://github.com/SimuladorDeFarm/Trabajo-grafos-2024.git
-
-#moverse a la carpeta del repositorio
-cd Trabajo-grafos-2024
-
 #crea entorno virtual
 echo "Creando entorno vitual..."
 python -m venv env
